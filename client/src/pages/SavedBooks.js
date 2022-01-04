0711de8728450b80f72a7729c5378fd5c3facbb6
Denis Arce <dnsarce21@gmail.com>
2022-01-03 23:53:36 -0600

    updated code

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -30,15 +30,8 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await removeBook({ variables: { bookId } });
+      const { data } = await removeBook({ variables: { bookId } });
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      // const updatedUser = await response.json();
-      // setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
